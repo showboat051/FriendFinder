f9d4e438fafac9cf6c388fb221fde5a2e07e4788
showboat051 <rashad.davis87@yahoo.com>
2018-05-03 21:58:41 -0500

    still needs lots of work

---
+++ app/data/questions.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {"What is your favorite sport?"},
+    {"Fried or baked?"},
+    {"Do you like spicy foods?"},
+    {"Are video games a form of art?"},
+    {"Are you more laid back or high-strung?"},
+    {"What is the golden age of gaming in your opinion?"},
+    {"Do you like soda?"},
+    {"Do you like things easy or challenging?"},
+    {"Bruce Lee or Jackie Chan?"},
+    {"Are you a nerd?"}
+
+];
+
+module.exports = questions;
